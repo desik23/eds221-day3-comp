1c22709e4e1f89f788dfefeebf2001f78a54eae9
desik23 <desik@bren.ucsb.edu>
2021-08-11 14:44:09 -0500

    saved

--- loops_exercise.R
+++ loops_exercise.R
@@ -82,8 +82,57 @@ library(tidyverse)
 library(palmerpenguins)
 
 penguin_summary <- penguins %>%
-  group_by(species) %>% 
+  group_by(species, island) %>% 
   summarize(mean_bill_depth = mean(bill_depth_mm, na.rm = TRUE),
             sd_bill_depth = sd(bill_depth_mm, na.rm = TRUE))
 
+penguin_subset <- penguins %>% 
+  ##filter is rows one, select is columns one
+  filter(island == "Torgersen") %>% 
+  select(species, flipper_length_mm, year)
 
+## Now, a for loops intro practice
+
+animal <- c("cat", "dog", "dog", "cat", "zebra", "elephant")
+
+for (i in seq_along(animal)) {
+  if (animal[i] == "dog") {
+    print("I love dogs!")
+  } else {
+    print("This is a different animal.")
+  }
+}
+
+
+species <- c("dog", "elephant", "goat", "dog", "dog", "elephant")
+age_human <- c(3, 8, 4, 12, 18)
+
+animal_ages <- vector(mode = "numeric", length = length(species))
+
+for (i in seq_along(species)) {
+  if (species[i] = "dog") {
+    animal_age <- age_human[i]*7
+  } else if (species[i] == "elephant") {
+    animal_age <- age_human[i] * 0.88
+  } else if (species[i] == "goat") {
+    animal_age <- age_human[i] * 4.7
+  }
+
+  animal_ages[i] <- animal_age  
+}
+
+
+
+mean_mtcars <- vector(mode = "numeric", length = ncol(mtcars))
+
+for (i in 1:ncol(mtcars)) {
+  mean_val <- mean(mtcars[[i]], na.rm = TRUE)
+}
+
+mean_mtcars 
+
+## Same thing with apply()
+
+apply(mtcars, 2, mean, na.rm = TRUE)
+
+map_df(mtcars, mean)
